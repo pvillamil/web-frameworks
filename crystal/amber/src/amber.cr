0afb3cc183c42bfb31bcfd712888b448cc3f0f91
Daniil Zhuravlev <nakrovati@outlook.com>
2026-02-28 02:40:15 +0400

    fix(crystal-amber): executable file not found

--- crystal/amber/src/amber.cr
+++ crystal/amber/src/amber.cr
@@ -1,12 +1,10 @@
-require "../config/*"
-
-Amber.env = "production"
+require "../config/application"
 
 if ARGV.size > 0 && ARGV[0] == "--start-amber"
   Amber::Server.start
 else
-  System.cpu_count.times do |i|
-    Process.new("/usr/src/app/bin/server", ["--start-amber"])
+  System.cpu_count.times do
+    Process.new(PROGRAM_NAME, ["--start-amber"])
   end
 end
 
