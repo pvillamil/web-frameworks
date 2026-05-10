effd3d9a512430d8c30272092f4bc9bac24119a1
patx <harrisonerd@gmail.com>
2026-05-10 14:31:31 -0400

    fix routing in micropie

--- python/micropie/server.py
+++ python/micropie/server.py
@@ -3,14 +3,13 @@ from micropie import App
 
 class Root(App):
 
-    async def homepage(self):
+    async def index(self):
         return ""
     
     async def user(self, user_id):
-        return user_id
-    
-    async def user(self):
         if self.request.method == "POST":
             return ""
+        return user_id
+
 
 app = Root()
