9b56053f68909d1b839a0af29def60c4db4df5c0
patx <harrisonerd@gmail.com>
2026-05-12 19:50:05 -0400

    improve quality of code for micropie and take advantage of its automatic mapping

--- python/micropie/server.py
+++ python/micropie/server.py
@@ -6,10 +6,7 @@ class Root(App):
     async def index(self):
         return ""
 
-    async def user(self, user_id=None):
-        if self.request.method == "POST":
-            return ""
+    async def user(self, user_id=""):
         return user_id
-
 
 app = Root()
